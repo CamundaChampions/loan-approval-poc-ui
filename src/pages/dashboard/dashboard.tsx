1a432e38daa9099d92a5fcd15bf23785b2430b0c
Ajay Narang <34938376+ajaynarang27@users.noreply.github.com>
2024-10-03 12:01:35 +0530

    dashboard page

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import PageTitle from '../../components/page_title/page_title';
+import { Table, TableHeader, TableRow, TableHeaderCell, TableCell, Button, Container, Grid, GridRow, GridColumn, Segment } from 'semantic-ui-react';
 
 const Dashboard = () => {
 
@@ -9,10 +10,100 @@ const Dashboard = () => {
         navigate('/apply_loan')
     }
 
+    const loanData = [
+        {
+            srNo: 1,
+            loanId: 3,
+            amount: "10,00,000",
+            term: 3,
+            status: "PENDING_INFO_CORRECTION",
+            loanReason: "Housing",
+            buttonLabel: "Open"
+        }
+    ]
+
     return (
         <div>
             <PageTitle title='Dashboard' />
-            <button onClick={handleClick} >Apply Loan</button>
+            <Grid columns={1}>
+                <GridRow>
+                    <GridColumn>
+                        <p>
+                            Welcome to <em>Loan Approval Application</em>
+                        </p>
+                    </GridColumn>
+                    <GridColumn>
+                        <p>
+                            Loan Request Assigned to you.
+                        </p>
+                    </GridColumn>
+                </GridRow>
+                <GridRow>
+                    <GridColumn>
+                        <Table celled>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHeaderCell>
+                                        Sr. No.
+                                    </TableHeaderCell>
+                                    <TableHeaderCell>
+                                        Loan ID
+                                    </TableHeaderCell>
+                                    <TableHeaderCell>
+                                        Amount
+                                    </TableHeaderCell>
+                                    <TableHeaderCell>
+                                        Term
+                                    </TableHeaderCell>
+                                    <TableHeaderCell>
+                                        Status
+                                    </TableHeaderCell>
+                                    <TableHeaderCell>
+                                        Loan Reason
+                                    </TableHeaderCell>
+                                <TableHeaderCell>
+                                    Button
+                                    </TableHeaderCell>
+                                </TableRow>
+                            </TableHeader>
+                            <tbody>
+                                {
+                                    loanData.map((loan) => (
+                                        <TableRow>
+                                        <TableCell>
+                                            {loan.srNo}
+                                        </TableCell>
+                                            <TableCell>
+                                                {loan.loanId}
+                                            </TableCell>
+                                            <TableCell>
+                                                {loan.amount}
+                                            </TableCell>
+                                            <TableCell>
+                                                {loan.term}
+                                            </TableCell>
+                                            <TableCell>
+                                                {loan.status}
+                                            </TableCell>
+                                            <TableCell>
+                                                {loan.loanReason}
+                                            </TableCell>
+                                            <TableCell>
+                                                <Button primary onClick={handleClick}>
+                                                    {loan.buttonLabel}
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </tbody>
+                        </Table>
+                    </GridColumn>
+                </GridRow>
+                
+            </Grid>
+
+            {/* <Button onClick={handleClick} >Apply Loan</Button> */}
         </div>
     )
 }
