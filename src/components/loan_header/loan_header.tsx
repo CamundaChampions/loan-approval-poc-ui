3390c0056b737f72dc6ced4769f916a8794a5e3a
Ajay Narang <34938376+ajaynarang27@users.noreply.github.com>
2024-10-04 12:47:10 +0530

    header

--- src/components/loan_header/loan_header.tsx
+++ src/components/loan_header/loan_header.tsx
@@ -1,9 +1,44 @@
+import Logo from '../../assets/img/genpact_logo.png';
+import { Grid, GridRow, GridColumn, Image, Header, Dropdown } from 'semantic-ui-react';
 import './loan_header.scss';
 
+
 const LoanHeader = () => {
+
+        const applicantsList = [
+            {
+                key: "Applicant_1",
+                text: "Applicant_1",
+                value: "Applicant_1"
+            },
+            {
+                key: "Applicant_2",
+                text: "Applicant_2",
+                value: "Applicant_2"
+            },
+            {
+                key: "Applicant_3",
+                text: "Applicant_3",
+                value: "Applicant_3"
+            }
+        ]
+    
     return (
         <div className='header'>
-            This is header
+
+            <Grid columns={3}>
+                <GridRow>
+                    <GridColumn>
+                        <Image src={Logo} size='small'/>
+                    </GridColumn>
+                    <GridColumn className='header_heading'>
+                        <Header as='h1'>Camunda POC</Header>
+                    </GridColumn>
+                    <GridColumn className='applicant_dropdown'>
+                        <Dropdown placeholder='Applicant' options={applicantsList} />
+                    </GridColumn>
+                </GridRow>
+            </Grid>
         </div>
     )
 }
