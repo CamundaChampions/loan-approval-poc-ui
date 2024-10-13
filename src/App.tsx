8bc7914de22a1d5ad3977c096e6e7bdbb43bd450
svegapatta <sureshvgknair@gmail.com>
2024-10-13 23:47:17 +0530

    CSS variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import './App.css';
+import './App.scss';
 import Routes from './routes/routes';
 import { store } from './store/store';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
